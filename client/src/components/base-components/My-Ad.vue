3ac91c938669028a8a7de3b3bd6a10d387d7e48d
castelain <2804755011@qq.com>
2019-03-13 18:41:14 +0800

    add ad close function

--- client/src/components/base-components/My-Ad.vue
+++ client/src/components/base-components/My-Ad.vue
@@ -1,7 +1,10 @@
 <template>
-    <router-link :to="ad.detailPath">
-        <img :src="ad.src" :alt="ad.title">
-    </router-link>
+    <div class="ad" v-show="isShow">
+        <router-link :to="ad.detailPath">
+            <img :src="ad.src" :alt="ad.title">
+        </router-link>
+        <i class="el-icon-close" title="关闭该广告" @click="closeAd"></i>
+    </div>
 </template>
 
 <script>
@@ -10,9 +13,14 @@ export default {
     props: [ 'ad' ],
     data () {
         return {
-
+            isShow: true
         }
-    }
+    },
+    methods: {
+        closeAd: function() {
+            this.isShow = false;
+        }
+    },
 }
 </script>
 
@@ -23,5 +31,16 @@ export default {
     img {
         width: 100%;
         overflow-y: hidden;
+    }
+    .ad {
+        position: relative;
+    } 
+    .el-icon-close {
+        position: absolute;
+        top: 0;
+        right: 0;
+        background-color:rgba(255, 255, 255, .2);
+        padding: 4px;
+        cursor: pointer;
     }
 </style>
